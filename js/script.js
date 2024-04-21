3031bf6d30a289dd1ee9711a390376a32f90b8e0
NeAnon <anontherper@gmail.com>
2024-04-21 14:44:33 +0200

    Add board clearing

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 function initializePage(){
 	console.log("Page initialized!");    
 }
+
+function createGrid(){
+	destroyGrid();
+}
+
+function destroyGrid(){
+	let board = document.getElementById("board");
+	while(board.firstChild){
+		console.log("Destroying child with ID " + board.firstChild.id + "!");
+		while(board.firstChild.firstChild){
+			console.log("Destroying child with ID " + board.firstChild.firstChild.id + "!");
+			board.firstChild.firstChild.remove();
+		}
+		board.firstChild.remove();
+	}
+}
